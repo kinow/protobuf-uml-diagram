01b3ff709c90679eca64c66d6c16fc3dcb28a60e
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-05-19 23:07:50 +1200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+# Copyright 2019 Bruno P. Kinoshita
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import codecs
+from os.path import join, dirname, abspath
+
+from setuptools import setup
+
+here = abspath(dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+install_requires = [
+    "click==7.0.*",
+    "graphviz==0.10.*",
+    "protobuf==3.7.*"
+]
+
+tests_require = [
+    'codecov==2.0.*',
+    'coverage==4.5.*',
+    'pytest-cov==2.6.*',
+    'pytest==4.4.*',
+    'pycodestyle==2.5.*'
+]
+
+setup(
+    version="0.1",
+    name="protobuf-uml-diagram",
+    description="Create UML diagrams from Protobuf proto files",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    scripts=["protobuf-uml-diagram"],
+    install_requires=install_requires,
+    tests_require=tests_require
+)
