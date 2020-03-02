22007d218ce3bff93f9358940a509c4feca36467
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-03-03 01:03:26 +1300

    Create relationship for #6

--- protobuf_uml_diagram.py
+++ protobuf_uml_diagram.py
@@ -101,9 +101,10 @@ def _process_descriptor(descriptor: Descriptor, classes: list,
             label = LABELS_BY_NUMBER[_field.label]
             if label == 'repeated':
                 # TODO: change relationship type issue #6
-                pass
+                relationships.append(f"    {that_node}->{this_node} [dir=backward;arrowhead=odiamond,arrowtail=normal;headlabel=\"1\";taillabel=\"0..*\"]")
+            else:
+                relationships.append(f"    {this_node}->{that_node} [arrowhead=none;headlabel=\"1\";taillabel=\"1\"]")
 
-            relationships.append(f"    {this_node}->{that_node}")
             field_type = that_node  # so we replace the 'message' token by the actual name
         else:
             field_type = TYPES_BY_NUMBER[_field.type]
