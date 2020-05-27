86f2daaa6eced2f308b959c55e3a9d4956c9702f
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-05-27 18:43:36 +1200

    Update setup.py version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ extras_require = {
 }
 
 setup(
-    version="0.5-dev",
+    version="0.5",
     name="protobuf-uml-diagram",
     description="Create UML diagrams from Protobuf proto files",
     long_description=open('README.md').read(),
